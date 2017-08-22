0b0d7de3d8790042c25d6737947d718991511b44
gwarner3 <george@georgedevelops.com>
2017-08-22 15:45:07 -0500

    Added Node class file with public properties and constructor.  Made BinaryTree class public.

---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        public int info;
+        public Node leftLink;
+        public Node rightLink;
+
+        public Node(int info)
+        {
+            this.info = info;
+            leftLink = null;
+            rightLink = null;
+        }
+    }
+}
