80b9dfe67f4dad91051d797f82affa9600d85874
gwarner3 <george@georgedevelops.com>
2017-08-22 16:24:02 -0500

    Renamed BinaryTree to BinarySearchTree

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTree
+    {
+        private Node root;
+        private string userResponse;
+
+        public BinarySearchTree()
+        {
+            root = null;
+        }
+        public void SelectionMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Key in number for what operation you ant to perform and press Enter.\n1. Add\n2. Search\n3. Exit application");
+            userResponse = Console.ReadLine();
+            switch (userResponse)
+            {
+                case "1":
+                    AddNodes();
+                    break;
+                case "2":
+                    SearchNodes();
+                    break;
+                case "3":                    
+                    Environment.Exit(1);
+                    break;
+            }            
+        }
+        public void AddNodes()
+        {
+
+        }
+        public void SearchNodes()
+        {
+
+        }
+    }
+}
