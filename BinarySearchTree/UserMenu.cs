79479e12a07ca491072b4333304f7640cdf25709
gwarner3 <george@georgedevelops.com>
2017-08-28 10:24:21 -0500

    Added try catch for varifying integer input from user

--- BinarySearchTree/UserMenu.cs
+++ BinarySearchTree/UserMenu.cs
@@ -13,7 +13,16 @@ namespace BinarySearchTree
         {
             Console.Clear();
             Console.WriteLine("Key in number for what operation you ant to perform and press Enter.\n1. Add\n2. Search\n3. Exit application");
-            userResponse = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                userResponse = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Please coose from one of the options. Press ENTER to continue.");
+                Console.ReadLine();
+                SelectionMenu();
+            }            
             return userResponse;
         }
         public static int GetNodeToAdd()
